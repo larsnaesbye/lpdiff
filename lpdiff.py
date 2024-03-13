9e132d9219867ae039bdb57fc3e3fd6f1d27ef1f
Lars Næsbye Christensen <lars@naesbye.dk>
2024-03-13 23:05:05 +0100

    fooling around with indices

--- lpdiff.py
+++ lpdiff.py
@@ -1,16 +1,17 @@
 import sys
-
 import requests
-from datetime import datetime
+import jsondiff as jd
+from jsondiff import diff
 
 # --- MAIN PROGRAM  ---------------------------------
-print(
-    datetime.fromtimestamp(datetime.now().timestamp()), " Starting lpdiff "
-)
-uri = sys.argv[1]
-response = requests.get(uri)
-print(response.json())
 
-print(
-    datetime.fromtimestamp(datetime.now().timestamp()), " Ending lpdiff "
-)
+urls = sys.argv[1:len(sys.argv)]
+jsons = []
+for url in urls:
+    response = requests.get(url)
+    # print(response.json())
+    jsons.append(response.json())
+for idj, j in enumerate(jsons, start=1):
+    result = jd.diff(jsons[idj], jsons[idj])
+    print(result)
+    # print(idj, j)
