5e9214e3f40a04d91160a6a2a2b3979be114e936
Lars Næsbye Christensen <lars@naesbye.dk>
2024-03-17 23:08:17 +0100

    Format

--- lpdiff.py
+++ lpdiff.py
@@ -1,18 +1,16 @@
 import sys
 import requests
 import jsondiff as jd
+import json
 from jsondiff import diff
-
-# --- MAIN PROGRAM  ---------------------------------
 
 urls = sys.argv[1:len(sys.argv)]
 jsons = []
 for url in urls:
     response = requests.get(url)
-    # print(response.json())
     jsons.append(response.json())
 for j in range(len(jsons)):
     for k in range(len(jsons)):
         result = jd.diff(jsons[j], jsons[k])
         if j < k:
-            print(result)
+            print(json.dumps(result, indent=0))
