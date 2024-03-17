6dcb7cf3051ee035a2227b9dacca9f1f2b2de579
Lars Næsbye Christensen <lars@naesbye.dk>
2024-03-17 22:57:15 +0100

    No duplicates

--- lpdiff.py
+++ lpdiff.py
@@ -11,7 +11,8 @@ for url in urls:
     response = requests.get(url)
     # print(response.json())
     jsons.append(response.json())
-for idj, j in enumerate(jsons, start=1):
-    result = jd.diff(jsons[idj], jsons[idj])
-    print(result)
-    # print(idj, j)
+for j in range(len(jsons)):
+    for k in range(len(jsons)):
+        result = jd.diff(jsons[j], jsons[k])
+        if j < k:
+            print(result)
